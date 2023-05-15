182ed0d50852a928809b08241411b8c054cf26be
Samurai-way <olegmoishevych@gmail.com>
2023-05-15 12:42:05 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+                                        // 1
+Array.prototype.last = function() {
+    if(this.length === 0) return -1
+    return this[this.length -1]
+};
+
+
+// const arr = [1, 2, 3]; should return 3
+// const arr = []; should return 0
+// console.log(arr.last());
