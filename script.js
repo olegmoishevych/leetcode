8b1b0caedf9f4c2a34f33d2e14273aef974670a8
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-07-23 13:13:40 +0300

    learn: LeetCode Problem #13

--- script.js
+++ script.js
@@ -58,35 +58,96 @@
  * @return {number[]}
  */
 
-//nums = [2,7,11,15], target = 9
-    // [0,1]
-const twoSum = (nums, target) => {
-        let map = new Map();
+// //nums = [2,7,11,15], target = 9
+//     // [0,1]
+// const twoSum = (nums, target) => {
+//         let map = new Map();
+//
+//         for (let i = 0; i < nums.length; i++) {
+//             if (nums[i] === target) {
+//                 return [i];
+//             }
+//             let complement = target - nums[i];
+//             if (map.has(complement)) {
+//                 return [map.get(complement), i];
+//             }
+//             map.set(nums[i], i);
+//         }
+//         return null
+//     };
+// // console.log(twoSum([2,7,11,15], 2))
+// // const arr = [1,2,3,4,5];
+// // const index = arr.map((el, index) => index)
+// // console.log(index)
+//
+// const arr = [2,7,11,15]
+// let map = new Map()
+// for (let i = 0; i < arr.length; i++) {
+//     map.set(i, arr[i])
+// }
+// console.log(map)
 
-        for (let i = 0; i < nums.length; i++) {
-            if (nums[i] === target) {
-                return [i];
-            }
-            let complement = target - nums[i];
-            if (map.has(complement)) {
-                return [map.get(complement), i];
-            }
-            map.set(nums[i], i);
+/**
+ * [LeetCode Problem #13](https://leetcode.com/problems/roman-to-integer/)
+ * Given a string s representing a Roman numeral, convert it to an integer.
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+ *
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ *
+ * For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+ * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+ *
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ *
+ * Example 1:
+ *
+ * Input: s = "III"
+ * Output: 3
+ * Explanation: III = 3.
+ * Example 2:
+ *
+ * Input: s = "LVIII"
+ * Output: 58
+ * Explanation: L = 50, V= 5, III = 3.
+ * Example 3:
+ *
+ * Input: s = "MCMXCIV"
+ * Output: 1994
+ * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 15
+ * s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+ * It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+ *
+ * @param {string} s
+ * @return {number}
+ */
+
+
+
+const romanToInt = (s) =>{
+    const roman = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000};
+
+    let total = 0
+
+    for (let i = 0; i < s.length; i++) {
+        if(roman[s[i]] < roman[s[i + 1]]){
+            total -= roman[s[i]]
+        }else {
+            total += roman[s[i]]
         }
-        return null
-    };
-// console.log(twoSum([2,7,11,15], 2))
-// const arr = [1,2,3,4,5];
-// const index = arr.map((el, index) => index)
-// console.log(index)
-
-const arr = [2,7,11,15]
-let map = new Map()
-for (let i = 0; i < arr.length; i++) {
-    map.set(i, arr[i])
-}
-console.log(map)
-
-
-
-
+    }
+    return total
+};
+console.log(romanToInt("MCMXCIV")); // Ожидается 1994
