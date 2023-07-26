b90c11e5cd07228b08b250f5794bc3316294b720
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-07-26 20:41:32 +0300

    learn: LeetCode Problem #14

--- script.js
+++ script.js
@@ -181,16 +181,16 @@ console.log(romanToInt("MCMXCIV")); // Ожидается 1994
  * @return {string}
  */
 
-const longestCommonPrefix = (strs) => {
-    if(strs.length === 0) return ''
-    let prefix = strs[0]
-    for (let i = 1; i < strs.length; i++) {
-        while (strs[i].indexOf(prefix) !== 0){
-            prefix = prefix.substring(0, prefix.length - 1)
-            if(prefix === "") return ""
-        }
-    }
-    return prefix
-};
-
-console.log(longestCommonPrefix(["flower","flow","flight"]))
+// const longestCommonPrefix = (strs) => {
+//     if(strs.length === 0) return ''
+//     let prefix = strs[0]
+//     for (let i = 1; i < strs.length; i++) {
+//         while (strs[i].indexOf(prefix) !== 0){
+//             prefix = prefix.substring(0, prefix.length - 1)
+//             if(prefix === "") return ""
+//         }
+//     }
+//     return prefix
+// };
+//
+// console.log(longestCommonPrefix(["flower","flow","flight"]))
