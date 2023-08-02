9d29d1815a491fa4d82b8e69eae00841073fae3e
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-08-02 16:52:07 +0300

    learn: LeetCode Problem #88

--- script.js
+++ script.js
@@ -313,18 +313,60 @@
  @return {boolean}
  */
 
-const containsDuplicate = (nums) => {
-    const actualNums = new Map()
-    let dublicates = 0
-    for (let i = 0; i < nums.length; i++) {
-        actualNums.set(nums[i], (actualNums.get(nums[i]) || 0) + 1)
-    }
-    actualNums.forEach(value => {
-        if(value >1){
-         dublicates++
-        }
-    })
-    return dublicates
-};
+// const containsDuplicate = (nums) => {
+//     const actualNums = new Map()
+//     let dublicates = 0
+//     for (let i = 0; i < nums.length; i++) {
+//         actualNums.set(nums[i], (actualNums.get(nums[i]) || 0) + 1)
+//     }
+//     actualNums.forEach(value => {
+//         if(value >1){
+//          dublicates++
+//         }
+//     })
+//     return dublicates
+// };
+//
+// console.log(containsDuplicate([1,2,3,4,5,2,3,4]))
 
-console.log(containsDuplicate([1,2,3,4,5,2,3,4]))
+/**
+
+ LeetCode Problem #88
+ You are given two integer arrays nums1 and nums2, both sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+ Your task is to merge nums1 and nums2 into a single array sorted in non-decreasing order. The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+ For example, if the input arrays are nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3, the output should be [1,2,2,3,5,6]. This is because the arrays we are merging are [1,2,3] and [2,5,6], and the result of the merge is [1,2,2,3,5,6].
+
+ If the input arrays are nums1 = [1], m = 1, nums2 = [], n = 0, the output should be [1]. This is because the arrays we are merging are [1] and [], and the result of the merge is [1].
+
+ If the input arrays are nums1 = [0], m = 0, nums2 = [1], n = 1, the output should be [1]. This is because the arrays we are merging are [] and [1], and the result of the merge is [1]. Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+ Example 1:
+ Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ Output: [1,2,2,3,5,6]
+ Explanation: The arrays we are merging are [1,2,3] and [2,5,6]. The result of the merge is [1,2,2,3,5,6].
+
+ Example 2:
+ Input: nums1 = [1], m = 1, nums2 = [], n = 0
+ Output: [1]
+ Explanation: The arrays we are merging are [1] and []. The result of the merge is [1].
+
+ Example 3:
+ Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+ Output: [1]
+ Explanation: The arrays we are merging are [] and [1]. The result of the merge is [1].
+
+ Constraints:
+ nums1.length == m + n
+ nums2.length == n
+ 0 <= m, n <= 200
+ 1 <= m + n <= 200
+ -10^9 <= nums1[i], nums2[j] <= 10^9
+
+ @param {number[]} nums1
+ @param {number} m
+ @param {number[]} nums2
+ @param {number} n
+ @return {void} Do not return anything, modify nums1 in-place instead.
+ */
