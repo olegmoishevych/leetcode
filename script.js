385c3ffa6134887533866aece45f1dd7dddbb55a
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-08-04 23:31:14 +0300

    learn: LeetCode Problem #2635 done

--- script.js
+++ script.js
@@ -85,3 +85,12 @@
  @return {number[]}
  */
 
+const map = function(arr, fn) {
+    const newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if(fn(arr[i], i)){
+            newArr.push(fn(arr[i],i))
+        }
+    }
+    return newArr
+};
