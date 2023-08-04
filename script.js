90c40247df5b4c4df1869c8ed9ca611405ecde63
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-08-04 23:46:55 +0300

    learn: Array.from(str)

--- script.js
+++ script.js
@@ -88,9 +88,13 @@
 // const map = function(arr, fn) {
 //     const newArr = []
 //     for (let i = 0; i < arr.length; i++) {
-//         if(fn(arr[i], i)){
-//             newArr.push(fn(arr[i],i))
-//         }
+//        newArr.push(fn(arr[i],i))
+//
 //     }
 //     return newArr
 // };
+
+const map = (arr, fn) => Array.from(arr, fn)
+
+const str = 'hello'
+console.log(Array.from(str))
