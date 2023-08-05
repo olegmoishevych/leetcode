0beaf459c92cd721634dbce07943c484bf02d4b3
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-08-05 23:07:50 +0300

    learn: Problem #2637

--- script.js
+++ script.js
@@ -98,3 +98,68 @@
 //
 // const str = 'hello'
 // console.log(Array.from(str))
+
+/**
+
+ Problem #2637
+ You are given an asynchronous function `fn` and a time `t` in milliseconds. Your task is to return a new time-limited version of the input function. The function `fn` takes arguments provided to the time-limited function.
+
+ The time-limited function should adhere to the following rules:
+
+ 1. If the `fn` completes within the time limit of `t` milliseconds, the time-limited function should resolve with the result.
+ 2. If the execution of the `fn` exceeds the time limit, the time-limited function should reject with the string "Time Limit Exceeded".
+
+ For example, if the input function `fn` is set to resolve after 100ms and the time limit `t` is set to 50ms, the time-limited function should reject because the time limit was reached before the function could resolve.
+
+ Example 1:
+ Input:
+ fn = async (n) => {
+   await new Promise(res => setTimeout(res, 100));
+   return n * n;
+ }
+ inputs = [5]
+ t = 50
+ Output: {"rejected":"Time Limit Exceeded","time":50}
+ Explanation: The provided function is set to resolve after 100ms. However, the time limit is set to 50ms. It rejects at t=50ms because the time limit was reached.
+
+ Example 2:
+ Input:
+ fn = async (n) => {
+   await new Promise(res => setTimeout(res, 100));
+   return n * n;
+ }
+ inputs = [5]
+ t = 150
+ Output: {"resolved":25,"time":100}
+ Explanation: The function resolved 5 * 5 = 25 at t=100ms. The time limit is never reached.
+
+ Example 3:
+ Input:
+ fn = async (a, b) => {
+   await new Promise(res => setTimeout(res, 120));
+   return a + b;
+ }
+ inputs = [5,10]
+ t = 150
+ Output: {"resolved":15,"time":120}
+ Explanation: The function resolved 5 + 10 = 15 at t=120ms. The time limit is never reached.
+
+ Example 4:
+ Input:
+ fn = async () => {
+   throw "Error";
+ }
+ inputs = []
+ t = 1000
+ Output: {"rejected":"Error","time":0}
+ Explanation: The function immediately throws an error.
+
+ Constraints:
+ 0 <= inputs.length <= 10
+ 0 <= t <= 1000
+ fn returns a promise
+ @param {function} fn
+ @param {number} t
+ @param {any[]} inputs
+ @return {Promise}
+ */
