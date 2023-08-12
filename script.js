2b8e194f931affa888648059ac8a9b347a8b4d0b
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-08-12 23:09:09 +0300

    learn: 2629. Function Composition done

--- script.js
+++ script.js
@@ -360,9 +360,14 @@
  * @param {Function[]} functions
  * @return {Function}
  */
-var compose = function(functions) {
+const compose = function(functions) {
     return function(x) {
-
+        if(functions.length === 0)
+            return x
+        for (let i = functions.length - 1; i >= 0; i--) {
+            x = functions[i](x)
+        }
+        return x
     }
 };
 
