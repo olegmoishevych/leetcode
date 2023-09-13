9f8c6dcfb4e8d3b565936066ca8f402208bd4b47
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-09-13 16:31:22 +0300

    learn: isSubsequence end

--- script.js
+++ script.js
@@ -1039,5 +1039,19 @@
  * @return {boolean}
  */
 let isSubsequence = function(s, t) {
+    let i = 0
+    let j = 0
 
+    while (j < t.length && i < s.length){
+        console.log(s[i])
+        console.log(t[j])
+
+        if(s[i] === t[j]){
+            i++
+        }
+        j++
+    }
+    return i === s.length
 };
+
+console.log(isSubsequence('aaaabaaaac', 'abc'))
