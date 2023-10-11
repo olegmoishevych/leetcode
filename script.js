ac3a737a8050e051ba58b8b41bdb8cf9628dadc5
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-10-11 22:27:57 +0300

    commit

--- script.js
+++ script.js
@@ -1726,29 +1726,29 @@
 // };
 // console.log(pivotIndex([1,7,3,6,5,6]))
 
-/**
- * @param {number[]} flowerbed
- * @param {number} n
- * @return {boolean}
- */
-let canPlaceFlowers = function(flowerbed, n) {
-    let count = 0
-
-    for (let i = 0; i < flowerbed.length; i++) {
-        if(!flowerbed[i] && !flowerbed[i - 1] && !flowerbed[i + 1]){
-            flowerbed[i] = 1
-            count++
-        }
-    }
-
-    return count >= n
-};
-
-// Input: flowerbed = [1,0,0,0,1], n = 1
-// Output: true
-
-// Input: flowerbed = [1,0,0,0,1], n = 2
-// Output: false
-// O(n), где n — это длина массива flowerbed
-
-console.log(canPlaceFlowers([1,0,0,0,1], 1))
+// /**
+//  * @param {number[]} flowerbed
+//  * @param {number} n
+//  * @return {boolean}
+//  */
+// let canPlaceFlowers = function(flowerbed, n) {
+//     let count = 0
+//
+//     for (let i = 0; i < flowerbed.length; i++) {
+//         if(!flowerbed[i] && !flowerbed[i - 1] && !flowerbed[i + 1]){
+//             flowerbed[i] = 1
+//             count++
+//         }
+//     }
+//
+//     return count >= n
+// };
+//
+// // Input: flowerbed = [1,0,0,0,1], n = 1
+// // Output: true
+//
+// // Input: flowerbed = [1,0,0,0,1], n = 2
+// // Output: false
+// // O(n), где n — это длина массива flowerbed
+//
+// console.log(canPlaceFlowers([1,0,0,0,1], 1))
