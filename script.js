56b41f9285e717fb82d0ccc8ab0806fef496af14
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-11-12 14:01:39 +0200

    1464 Maximum Product of Two Elements in an Array

--- script.js
+++ script.js
@@ -652,3 +652,27 @@
 //     return res
 // }
 // console.log(clearDoubleOfArr([1,2,3,4,5,5,6,6]))
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+let maxProduct = function(nums) {
+    let max1 = nums[0]
+    let max2 = -Infinity
+
+    for (let i = 1; i < nums.length; i++) {
+        if(nums[i] > max1){
+            max2 = max1
+            max1 = nums[i]
+        }
+
+        else if(nums[i] > max2){
+            max2 = nums[i]
+        }
+    }
+
+
+    return (max1 - 1) * (max2 - 1)
+};
+console.log(maxProduct([1,5,4,5,6]))
