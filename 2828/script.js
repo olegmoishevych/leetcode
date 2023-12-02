c7935717ab84dfeb41812be33f451d50d86158f4
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 22:58:51 +0200

    2828

--- 2828/script.js
+++ 2828/script.js
@@ -36,7 +36,16 @@
  * @return {boolean}
  */
 let isAcronym = function(words, s) {
+  if(words.length !== s.length){
+    return false
+  }
+
+  for (let i = 0; i < words.length; i++) {
+     if(words[i][0] !== s[i]) return false
+  }
+
+  return true
 
 };
 
-console.log(isAcronym(["alice","bob","charlie"], 'abc'))
+console.log(isAcronym(["never","gonna","give","up","on","you"], 'ngguoy'))
