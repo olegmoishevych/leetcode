10cf14910c15f6ea679c781bdc907a37b75a2eca
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-15 22:36:41 +0200

    separateDigits

--- 1/script.js
+++ 1/script.js
@@ -1,12 +1,19 @@
 // aba = true
 // abc = false
 
-const foo = (str) => {
-  return str.split('').join() === str.split('').reverse().join()
-}
+// const foo = (str) => {
+//   return str.split('').join() === str.split('').reverse().join()
+// }
+//
+// console.log(foo('aba'))
+//
+// let arr = ['1,2,3,4,5']
+// let copy = [...arr]
+// console.log(...copy)
 
-console.log(foo('aba'))
+let arr1 = [1,2,3]
+let arr2 = [4,5,6]
 
-let arr = ['1,2,3,4,5']
-let copy = [...arr]
-console.log(...copy)
+let arr3 = [...arr1, ...arr2]
+
+console.log(arr3)
