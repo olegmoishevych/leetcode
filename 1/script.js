396c6cd9681d287c3cc68e02e52c57c6b92265ae
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-16 22:08:44 +0200

    2744

--- 1/script.js
+++ 1/script.js
@@ -11,9 +11,23 @@
 // let copy = [...arr]
 // console.log(...copy)
 
-let arr1 = [1,2,3]
-let arr2 = [4,5,6]
+// let arr1 = [1,2,3]
+// let arr2 = [4,5,6]
+//
+// let arr3 = [...arr1, ...arr2]
+//
+// console.log(arr3)
 
-let arr3 = [...arr1, ...arr2]
+//Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+//
+// Examples:
+// Input: 42145 Output: 54421
+//
+// Input: 145263 Output: 654321
+//
+// Input: 123456789 Output: 987654321
 
-console.log(arr3)
+let descendingOrder = (n) => +n.toString().split('').sort((a,b)=> b - a).join('')
+
+
+console.log(descendingOrder(42145))
