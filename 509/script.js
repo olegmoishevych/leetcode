acdf5ef943946b0f911d878c82f76e7c021ad3d4
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-22 23:18:59 +0200

    Fibonacci

--- 509/script.js
+++ 509/script.js
@@ -27,7 +27,7 @@
  * @param {number} n
  * @return {number}
  */
-var fib = (n) => n <= 1 ? n : fib ( n - 1 ) + fib( n - 2 )
+var fib = (n) => n <= 1 ? n : fib( n - 1 ) + fib( n - 2 )
 
 console.log(fib(4))
 
