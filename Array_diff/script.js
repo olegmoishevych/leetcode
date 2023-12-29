ae21adcb8574622dcb8af2b0bbc1dbb44b803791
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-29 19:11:27 +0200

    arrayDiff

---
+++ Array_diff/script.js
@@ -0,0 +1,13 @@
+//Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+// It should remove all values from list a which are present in list b keeping their order.
+
+function arrayDiff(a, b) {
+  let setB = new Set(b)
+
+  return a.filter((el) => !setB.has(el))
+}
+
+console.log(arrayDiff([1,2,2], [1]))
+
+//assert.deepEqual(arrayDiff([1,2], [1]), [2], "a was [1,2], b was [1]");
+//assert.deepEqual(arrayDiff([1,2,2], [1]), [2,2], "a was [1,2,2], b was [1]");
