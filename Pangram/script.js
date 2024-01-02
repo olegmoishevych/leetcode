e12c68831db6cd1a2d106bb9b1bb297803e9f1dd
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-01-02 21:06:09 +0200

    isPangram (2 version)

--- Pangram/script.js
+++ Pangram/script.js
@@ -1,15 +1,9 @@
 //A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
 // Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 
-function isPangram(string){
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+const isPangram = (string) => [...'abcdefghijklmnopqrstuvwxyz'].every(el => string.toLowerCase().includes(el))
 
-  const str = string.toLowerCase().trim()
-
-  return [...alphabet].every((el) => str.includes(el))
-}
-
-console.log(isPangram('This is not a pangram.'))
+console.log(isPangram('The quick brown fox jumps over the lazy dog.'))
 
 //const chai = require("chai");
 // const assert = chai.assert;
