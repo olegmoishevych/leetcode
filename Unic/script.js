b238462d04e9b736f609ab0793748cad25d96458
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-01-04 23:15:30 +0200

    findUniqNum (2 ver)

--- Unic/script.js
+++ Unic/script.js
@@ -1,11 +1,10 @@
 //[4, 1, 2, 1, 2]
 
 const findUniqNum = (num) => {
+
   for (const n of num) {
 
-    if(num.indexOf(n) === num.lastIndexOf(n)){
-      return parseInt(n, 10)
-    }
+    if(num.indexOf(n) === num.lastIndexOf(n)) return n
 
   }
 }
