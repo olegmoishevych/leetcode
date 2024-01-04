e3493786631e55404f904ca83bb76583362ac4c3
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-01-04 22:14:34 +0200

    maximumValue

---
+++ 2496/script.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} strs
+ * @return {number}
+ */
+var maximumValue = function(strs) {
+  let length = strs.map(item => {
+    if (!isNaN(item) && !Array.isArray(item)) {
+      return parseInt(item,10)
+    }
+
+    return item.length
+
+  });
+
+  return Math.max(...length)
+};
+
+console.log(maximumValue(["alic3","bob","3","4","00000"]))
+//Example 1:
+// Input: strs = ["alic3","bob","3","4","00000"]
+// Output: 5
+
+//Example 2:
+// Input: strs = ["1","01","001","0001"]
+// Output: 1
