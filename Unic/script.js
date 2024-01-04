4c57eb9f61bde12922f7ff62973a4e77c8efc12d
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-01-04 23:18:54 +0200

    findUniqNum (3 ver)

--- Unic/script.js
+++ Unic/script.js
@@ -3,10 +3,13 @@
 const findUniqNum = (num) => {
 
   for (const n of num) {
-
+    console.log(num.lastIndexOf(n))
+    console.log(num.indexOf(n))
     if(num.indexOf(n) === num.lastIndexOf(n)) return n
 
   }
+
+  return 0
 }
 
 console.log(findUniqNum([4,1,2,1,2]))
