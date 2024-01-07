eacea16a74882802d10dbf503707cf203e5a96bd
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-01-07 23:03:20 +0200

    findDifference (1 variant)

---
+++ 2215/script.js
@@ -0,0 +1,43 @@
+//Example 1:
+//
+// Input: nums1 = [1,2,3], nums2 = [2,4,6]
+// Output: [[1,3],[4,6]]
+// Explanation:
+// For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
+// For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4,6].
+// Example 2:
+//
+// Input: nums1 = [1,2,3,3], nums2 = [1,1,2,2]
+// Output: [[3],[]]
+// Explanation:
+// For nums1, nums1[2] and nums1[3] are not present in nums2. Since nums1[2] == nums1[3], their value is only included once and answer[0] = [3].
+// Every integer in nums2 is present in nums1. Therefore, answer[1] = [].
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+  let n1Set = new Set()
+  let n2Set = new Set()
+
+
+  for (const n1 of nums1) {
+
+    if(!nums2.includes(n1)){
+      n1Set.add(n1)
+    }
+
+  }
+
+  for (const n2 of nums2) {
+    if(!nums1.includes(n2)){
+      n2Set.add(n2)
+    }
+  }
+
+  return [[...n1Set], [...n2Set]]
+};
+
+console.log(findDifference([1,2,3], [2,4,6]))
