a2d52f7c336c53909b28a47242aefdb35c1f3d50
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-01-08 19:46:28 +0200

    findNumbers (2_v)

--- 1295/script.js
+++ 1295/script.js
@@ -19,18 +19,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findNumbers = function(nums) {
-  let count = 0
+// var findNumbers = function(nums) {
+//   let count = 0
+//
+//   for (let i = 0; i < nums.length; i++) {
+//
+//     if(nums[i].toString().length % 2 === 0){
+//       count++
+//     }
+//
+//   }
+//
+//   return count
+// };
 
-  for (let i = 0; i < nums.length; i++) {
-
-    if(nums[i].toString().length % 2 === 0){
-      count++
-    }
-
-  }
-
-  return count
-};
+const findNumbers = (nums) => nums.filter((e) => e.toString().length % 2 === 0).length
 
 console.log(findNumbers([12,345,2,6,7896]))
