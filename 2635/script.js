1211cf1e1efdd9beba028104bc13a14959d8dec1
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-01-10 12:26:47 +0200

    map (2 vers)

--- 2635/script.js
+++ 2635/script.js
@@ -21,16 +21,17 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function(arr, fn) {
-  let res = []
-
-  for (let i = 0; i < arr.length; i++) {
-
-    res.push(fn(arr[i], i))
-
-  }
-
-  return res
-};
+// var map = function(arr, fn) {
+//   let res = []
+//
+//   for (let i = 0; i < arr.length; i++) {
+//
+//     res.push(fn(arr[i], i))
+//
+//   }
+//
+//   return res
+// };
+const map = (arr, fn) => arr.map((element, index) => fn(element, index));
 
 console.log(map([1,2,3], function plusone(n) { return n + 1; }))
