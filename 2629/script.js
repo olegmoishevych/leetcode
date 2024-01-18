55c13ee548e6c8157a5225b7f119ed4f1d8f88f6
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-01-18 20:31:25 +0200

    compose 2v

--- 2629/script.js
+++ 2629/script.js
@@ -28,16 +28,18 @@
  * @param {Function[]} functions
  * @return {Function}
  */
-var compose = function(functions){
-  return function(x) {
+// var compose = function(functions){
+//   return function(x) {
+//
+//     for (let i = functions.length - 1; i >= 0 ; i--) {
+//       x = functions[i](x)
+//     }
+//
+//     return x
+//   }
+// }
+const compose = functions => x => functions.reduceRight((acc, fn) => fn(acc), x);
 
-    for (let i = functions.length - 1; i >= 0 ; i--) {
-      x = functions[i](x)
-    }
-
-    return x
-  }
-}
 const fn = compose([x => x + 1, x => 2 * x])
 console.log(compose(4))
 
