f02240999f9d95455cc3005eb3185594564202c0
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-01-21 19:54:01 +0200

    moveZeroes description

---
+++ 283/script.js
@@ -0,0 +1,19 @@
+//Пример 1:
+//
+// Ввод: числа = [0,1,0,3,12]
+//  Вывод: [1,3,12,0,0]
+// Пример 2:
+//
+// Ввод: числа = [0]
+//  Вывод: [0]
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const moveZeroes = (nums) => {
+
+
+}
+
+console.log(moveZeroes([0,1,0,3,12]))
