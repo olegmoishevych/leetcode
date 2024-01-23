e406fb607903f809f263f6182da02f508f74fa81
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-01-23 18:29:03 +0200

    singleNumber (1 vers)

---
+++ 136/script.js
@@ -0,0 +1,35 @@
+//Example 1:
+//
+// Input: nums = [2,2,1]
+// Output: 1
+// Example 2:
+//
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+// Example 3:
+//
+// Input: nums = [1]
+// Output: 1
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = (nums) => {
+  return nums.filter((el) => {
+    let count = 0
+    for (let i = 0; i < nums.length; i++) {
+
+      if(nums[i] === el){
+        count++
+      }
+
+    }
+
+    return count === 1
+
+  })
+
+}
+
+console.log(singleNumber([4,1,2,1,2]))
