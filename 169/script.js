d561de9e8897af1c3054a85de0e9cfc1c778c853
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-01-24 22:33:22 +0200

    majorityElement (1 vers)

---
+++ 169/script.js
@@ -0,0 +1,30 @@
+//Example 1:
+// Input: nums = [3,2,3]
+// Output: 3
+
+// Example 2:
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const majorityElement = (nums) => {
+  let count = 0
+  let candidate
+
+  for (let i = 0; i < nums.length; i++) {
+
+    if(count === 0){
+      candidate = nums[i]
+    }
+
+    count += (candidate === nums[i]) ? 1 : -1
+  }
+  console.log(candidate)
+
+  return candidate
+}
+
+console.log(majorityElement([8,8,7,7,7,8]))
