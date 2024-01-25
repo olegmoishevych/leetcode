3f391468ec96679e976b0b79ac3e094e0c708b7a
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-01-25 20:38:51 +0200

    once

---
+++ 2666/script.js
@@ -0,0 +1,46 @@
+//Example 1:
+//
+// Input: fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
+// Output: [{"calls":1,"value":6}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(1, 2, 3); // 6
+// onceFn(2, 3, 6); // undefined, fn was not called
+// Example 2:
+//
+// Input: fn = (a,b,c) => (a * b * c), calls = [[5,7,4],[2,3,6],[4,6,8]]
+// Output: [{"calls":1,"value":140}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(5, 7, 4); // 140
+// onceFn(2, 3, 6); // undefined, fn was not called
+// onceFn(4, 6, 8); // undefined, fn was not called
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+  let count = 0
+
+  return function(...args){
+
+      if(count === 0){
+        count++
+        return fn(...args)
+      }
+    return undefined
+  }
+
+};
+
+let fn = (a,b,c) => (a + b + c)
+console.log(once(fn))
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
