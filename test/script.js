9aee2d6effa0497d841d527c44e4e7cffda77202
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-01-30 13:59:22 +0200

    func (1 vers)

---
+++ test/script.js
@@ -0,0 +1,43 @@
+const arr = [
+  {
+    id: "a",
+    name: "Ant",
+    surname: "Bon"
+  },
+  {
+    id: "b",
+    name: "Bont",
+    surname: "Cion"
+  },
+  {
+    id: "c",
+    name: "Fon",
+  },
+  {
+    id: "d"
+  }
+];
+
+const obj = {
+  a: {
+    name: "Ant",
+    surname: "Bon"
+  },
+  b: {
+    name: "Bont",
+    surname: "Cion"
+  },
+  c: {
+    name: "Fon",
+  },
+  d: null
+}
+
+const func = (arr) => arr.reduce((acc, cur) => {
+  const {id, ...rest} = cur
+  acc[id] = Object.keys(rest).length ? rest : null
+
+  return acc
+}, {})
+console.log(func(arr))
+// {} отримати обж з айдішками,
