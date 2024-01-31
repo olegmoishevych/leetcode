e018dcc136ab85b4faa989471c861d61713f2bf8
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-01-31 21:46:57 +0200

    prefixCount (1 version)

---
+++ 2185/script.js
@@ -0,0 +1,30 @@
+//Example 1:
+//
+// Input: words = ["pay","attention","practice","attend"], pref = "at"
+// Output: 2
+// Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
+// Example 2:
+//
+// Input: words = ["leetcode","win","loops","success"], pref = "code"
+// Output: 0
+// Explanation: There are no strings that contain "code" as a prefix.
+
+/**
+ * @param {string[]} words
+ * @param {string} pref
+ * @return {number}
+ */
+const prefixCount = (words, pref) => {
+  let count = 0
+
+  for (let i = 0; i < words.length; i++) {
+    console.log(words[i])
+    if(words[i].startsWith(pref)){
+      count++
+    }
+  }
+
+  return count
+}
+
+console.log(prefixCount( ["pay","attention","practice","attend"],"at"))
