c4ed5e4432decd7856baf9cb199f22607c963c4a
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-01-31 21:54:02 +0200

    countPrefixes (1vers)

---
+++ 2255/script.js
@@ -0,0 +1,34 @@
+//Example 1:
+//
+// Input: words = ["a","b","c","ab","bc","abc"], s = "abc"
+// Output: 3
+// Explanation:
+// The strings in words which are a prefix of s = "abc" are:
+// "a", "ab", and "abc".
+// Thus the number of strings in words which are a prefix of s is 3.
+// Example 2:
+//
+// Input: words = ["a","a"], s = "aa"
+// Output: 2
+// Explanation:
+// Both of the strings are a prefix of s.
+// Note that the same string can occur multiple times in words, and it should be counted each time.
+
+/**
+ * @param {string[]} words
+ * @param {string} s
+ * @return {number}
+ */
+const countPrefixes = (words, s) => {
+  let count = 0
+
+  for (let i = 0; i < words.length; i++) {
+    if(s.startsWith(words[i])){
+      count++
+    }
+  }
+
+  return count
+}
+
+console.log(countPrefixes(["a","b","c","ab","bc","abc"], 'abc'))
