e80170f6be032ec702ac16ac1a0b534752daf15f
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-02-06 22:15:18 +0200

    firstUniqChar (1 version)

---
+++ 387/script.js
@@ -0,0 +1,36 @@
+//Example 1:
+//
+// Input: s = "leetcode"
+// Output: 0
+// Example 2:
+//
+// Input: s = "loveleetcode"
+// Output: 2
+// Example 3:
+//
+// Input: s = "aabb"
+// Output: -1
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const firstUniqChar = (s) => {
+  let obj = {}
+
+  for (const char of s) {
+    obj[char] = (obj[char] || 0) +1
+  }
+
+  for (let i = 0; i < s.length; i++) {
+
+    if(obj[s[i]] === 1){
+      return i
+    }
+
+  }
+
+  return -1
+}
+
+console.log(firstUniqChar('loveleetcode'))
