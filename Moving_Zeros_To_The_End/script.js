a9067ca25b00af6236363b0a7ba92985fbc81308
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-02-07 21:41:03 +0200

    moveZeros (1 variant)

---
+++ Moving_Zeros_To_The_End/script.js
@@ -0,0 +1,18 @@
+//Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+//moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+const moveZeros = (arr) => {
+  let zeros = []
+
+  for (const zero of arr) {
+
+    if(zero === 0){
+      zeros.push(zero)
+    }
+
+  }
+
+  return arr.filter((el) => el !== 0).concat(zeros)
+}
+
+console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]))
