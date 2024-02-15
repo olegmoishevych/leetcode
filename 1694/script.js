52de422dda379ebb8174066f6dddfe11785cc649
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-02-15 23:02:05 +0200

    reformatNumber

---
+++ 1694/script.js
@@ -0,0 +1,42 @@
+//Example 1:
+//
+// Input: number = "1-23-45 6"
+// Output: "123-456"
+// Explanation: The digits are "123456".
+// Step 1: There are more than 4 digits, so group the next 3 digits. The 1st block is "123".
+// Step 2: There are 3 digits remaining, so put them in a single block of length 3. The 2nd block is "456".
+// Joining the blocks gives "123-456".
+// Example 2:
+//
+// Input: number = "123 4-567"
+// Output: "123-45-67"
+// Explanation: The digits are "1234567".
+// Step 1: There are more than 4 digits, so group the next 3 digits. The 1st block is "123".
+// Step 2: There are 4 digits left, so split them into two blocks of length 2. The blocks are "45" and "67".
+// Joining the blocks gives "123-45-67".
+// Example 3:
+//
+// Input: number = "123 4-5678"
+// Output: "123-456-78"
+// Explanation: The digits are "12345678".
+// Step 1: The 1st block is "123".
+// Step 2: The 2nd block is "456".
+// Step 3: There are 2 digits left, so put them in a single block of length 2. The 3rd block is "78".
+// Joining the blocks gives "123-456-78".
+
+/**
+ * @param {string} number
+ * @return {string}
+ */
+/**
+ * @param {string} number
+ * @return {string}
+ */
+const reformatNumber = (number) => {
+  const digitsOnly = number.replace(/\D/g, "");
+  // Split num for group at 2 and 3 nums
+  return digitsOnly.replace(/(...?(?=..))/g, "$1-");
+};
+
+
+console.log(reformatNumber("123 4-567"))
