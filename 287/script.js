e15b17f556a0d76a398d160eb9fdb37d10779a23
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-02-23 22:21:05 +0200

    findDuplicate

---
+++ 287/script.js
@@ -0,0 +1,30 @@
+//Example 1:
+//
+// Input: nums = [1,3,4,2,2]
+// Output: 2
+// Example 2:
+//
+// Input: nums = [3,1,3,4,2]
+// Output: 3
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findDuplicate = (nums) => {
+  let set = new Set()
+  let n
+
+  for (const num of nums) {
+
+    if(set.has(num)){
+      n = num
+    }
+
+    set.add(num)
+  }
+
+  return n
+}
+
+console.log(findDuplicate([1,3,4,2,2]))
