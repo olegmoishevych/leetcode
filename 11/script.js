a11c10a4c907d95f8b31dc7062c3ea0d030c3b4e
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-02-25 21:59:21 +0200

    findOdd final version

--- 11/script.js
+++ 11/script.js
@@ -5,20 +5,22 @@
 // [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 // [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
-const findOdd = (A) => {
-  let count = {}
+// const findOdd = (A) => {
+//   let count = {}
+//
+//   for (const a of A) {
+//     count[a] = (count[a] || 0) + 1
+//   }
+//
+//   for (const num in count) {
+//
+//     if(count[num] % 2 !== 0){
+//       return +num
+//     }
+//
+//   }
+// }
 
-  for (const a of A) {
-    count[a] = (count[a] || 0) + 1
-  }
+const findOdd = (arr) => arr.reduce((a,b) => a ^ b)
 
-  for (const num in count) {
-
-    if(count[num] % 2 !== 0){
-      return +num
-    }
-
-  }
-}
-
-console.log(findOdd([0,1,0,1,0]))
+console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]))
