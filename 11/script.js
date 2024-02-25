386bfd45a02a2f157b40463a9a217aad3a0399e3
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-02-25 21:58:33 +0200

    findOdd 1v

---
+++ 11/script.js
@@ -0,0 +1,24 @@
+//Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+const findOdd = (A) => {
+  let count = {}
+
+  for (const a of A) {
+    count[a] = (count[a] || 0) + 1
+  }
+
+  for (const num in count) {
+
+    if(count[num] % 2 !== 0){
+      return +num
+    }
+
+  }
+}
+
+console.log(findOdd([0,1,0,1,0]))
