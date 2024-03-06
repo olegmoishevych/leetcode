cff18a4600f7ad72654da5b62c80aa63e288aca8
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-03-06 22:49:04 +0200

    solution

--- 2/script.js
+++ 2/script.js
@@ -1,18 +1,18 @@
 // * 'abc' =>  ['ab', 'c_']
 // * 'abcdef' => ['ab', 'cd', 'ef']
 
-const solution = (str) => {
-  let result = []
-
-  for ( let i = 0; i < str.length; i+=2 ) {
-    result.push(str.substring(i, i + 2))
-  }
-
-  if(result.length > 0 && result[result.length - 1].length === 1){
-    result[result.length - 1] += '_'
-  }
-
-  return result
-}
+// const solution = (str) => {
+//   let result = []
+//
+//   for ( let i = 0; i < str.length; i+=2 ) {
+//     result.push(str.substring(i, i + 2))
+//   }
+//
+//   if(result.length > 0 && result[result.length - 1].length === 1){
+//     result[result.length - 1] += '_'
+//   }
+//
+//   return result
+// }
 
 console.log(solution('hello'))
