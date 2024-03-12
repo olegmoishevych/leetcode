626c8f74c5c00fc506bd7e7e129573abfc1b98e4
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-03-12 22:25:26 +0200

    largestAltitude

---
+++ 1732/script.js
@@ -0,0 +1,28 @@
+//Example 1:
+//
+// Input: gain = [-5,1,5,0,-7]
+// Output: 1
+// Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+// Example 2:
+//
+// Input: gain = [-4,-3,-2,-1,4,3,2]
+// Output: 0
+// Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+const largestAltitude = (gain) => {
+  const arr = [0]
+
+  for(let i=0; i < gain.length; i++){
+    const el = gain[i]+arr[arr.length-1]
+    arr.push(el)
+
+  }
+
+  return Math.max(...arr)
+}
+
+console.log(largestAltitude([-5,1,5,0,-7]))
