7426e87145ba5755ca9a8e15f25ecaab9263b214
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-03-17 23:26:28 +0200

    commit

--- 21/script.js
+++ 21/script.js
@@ -30,7 +30,6 @@
 // The number of nodes in both lists is in the range [0, 50].
 // -100 <= Node.val <= 100
 // Both list1 and list2 are sorted in non-decreasing order.
-
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
