53da27b078ea6b0be802fc8e84d73fb5efa5f8cc
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-03-30 21:36:14 +0200

    addStrings

--- 415/script.js
+++ 415/script.js
@@ -11,10 +11,10 @@
 // Input: num1 = "0", num2 = "0"
 // Output: "0"
 
-/**
- * @param {string} num1
- * @param {string} num2
- * @return {string}
- */
+// /**
+//  * @param {string} num1
+//  * @param {string} num2
+//  * @return {string}
+//  */
 const addStrings = (num1, num2) => (BigInt(num1) + BigInt(num2)).toString()
 console.log(addStrings("456", "77"))
