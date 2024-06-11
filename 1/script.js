885a2bc92c4088c3542cef270faa6d3c75f5d619
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-06-11 23:38:26 +0300

    twoSum fix

--- 1/script.js
+++ 1/script.js
@@ -84,14 +84,12 @@
 // }
 //
 // console.log(foo('the', 'the is'))
-
 const strCounter = (str) => {
   let counter = {}
 
   for (const char of str) {
     counter[char] = (counter[char] || 0) + 1
   }
-
   return counter
 }
 
