006560eab0288e5635efdf675077881e4f757884
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-07-24 18:49:17 +0300

    destrict

---
+++ dest/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+const user = {
+  name: "Alice",
+  age: 25,
+  email: "alice@example.com"
+};
+
+const { name, age, email } = user;
+
+console.log(name);
+console.log(age);
+console.log(email);
