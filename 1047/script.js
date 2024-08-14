8b3da5d023d6bcbc0129f797ee03111523e0beff
Oleh Moishevych <93545466+olegmoishevych@users.noreply.github.com>
2024-08-14 23:30:12 +0300

    Update script.js

--- 1047/script.js
+++ 1047/script.js
@@ -1,18 +1,3 @@
-//Example 1:
-//
-// Input: s = "abbaca"
-// Output: "ca"
-// Explanation:
-// For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
-// Example 2:
-//
-// Input: s = "azxxzy"
-// Output: "ay"
-
-/**
- * @param {string} s
- * @return {string}
- */
 const removeDuplicates = (s) => {
   let stack = []
 
