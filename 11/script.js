d1fe59ea057a6054dc75bd94f07880db40943401
Oleh Moishevych <93545466+olegmoishevych@users.noreply.github.com>
2024-08-16 23:07:27 +0300

    Update script.js

--- 11/script.js
+++ 11/script.js
@@ -1,4 +1,4 @@
-//Examples
+
 // [7] should return 7, because it occurs 1 time (which is odd).
 // [0] should return 0, because it occurs 1 time (which is odd).
 // [1,1,2] should return 2, because it occurs 1 time (which is odd).
