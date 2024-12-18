e4ad98dd1e728a38cc0617445cf492b8a8d89345
Oleh Moishevych <93545466+olegmoishevych@users.noreply.github.com>
2024-12-18 22:20:06 +0200

    Update script.js

--- 2215/script.js
+++ 2215/script.js
@@ -5,13 +5,11 @@
 // For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
 // For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4,6].
 // Example 2:
-//
 // Input: nums1 = [1,2,3,3], nums2 = [1,1,2,2]
 // Output: [[3],[]]
 // Explanation:
 // For nums1, nums1[2] and nums1[3] are not present in nums2. Since nums1[2] == nums1[3], their value is only included once and answer[0] = [3].
 // Every integer in nums2 is present in nums1. Therefore, answer[1] = [].
-
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
