e65b4f97c8f5ec34a4678088f13f1710e9ec6734
Oleh Moishevych <93545466+olegmoishevych@users.noreply.github.com>
2024-12-22 18:20:21 +0200

    Update script.js

--- 557/script.js
+++ 557/script.js
@@ -3,10 +3,8 @@
 // Input: s = "Let's take LeetCode contest"
 // Output: "s'teL ekat edoCteeL tsetnoc"
 // Example 2:
-//
 // Input: s = "Mr Ding"
 // Output: "rM gniD"
-
 /**
  * @param {string} s
  * @return {string}
