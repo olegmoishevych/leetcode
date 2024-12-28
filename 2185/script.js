9dc22ba133c32b11f3e3ec898f4063c71ea2891d
Oleh Moishevych <93545466+olegmoishevych@users.noreply.github.com>
2024-12-28 22:04:39 +0200

    Update script.js

--- 2185/script.js
+++ 2185/script.js
@@ -1,5 +1,4 @@
 //Example 1:
-
 // Input: words = ["pay","attention","practice","attend"], pref = "at"
 // Output: 2
 // Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
