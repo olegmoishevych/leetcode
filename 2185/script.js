d1796c3efa04052a1d559f3409ae7ac1651adeb6
Oleh Moishevych <93545466+olegmoishevych@users.noreply.github.com>
2025-01-19 21:14:07 +0200

    Update script.js

--- 2185/script.js
+++ 2185/script.js
@@ -3,7 +3,6 @@
 // Output: 2
 // Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
 // Example 2:
-//
 // Input: words = ["leetcode","win","loops","success"], pref = "code"
 // Output: 0
 // Explanation: There are no strings that contain "code" as a prefix.
