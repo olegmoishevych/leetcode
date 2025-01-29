d9c70fe5e32326af27cd3f69bcf112378f46fbf3
Oleh Moishevych <93545466+olegmoishevych@users.noreply.github.com>
2025-01-29 22:18:08 +0200

    Update script.js

--- 1832/script.js
+++ 1832/script.js
@@ -4,7 +4,6 @@
 // Companies
 // Hint
 // A pangram is a sentence where every letter of the English alphabet appears at least once.
-//
 // Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
 //
 //
