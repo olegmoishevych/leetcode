18722af879e515425723928f6f12b18566aa54cf
Oleh Moishevych <93545466+olegmoishevych@users.noreply.github.com>
2025-02-01 22:42:29 +0200

    Update script.js

--- 268/script.js
+++ 268/script.js
@@ -12,7 +12,6 @@
 // Input: nums = [9,6,4,2,3,5,7,0,1]
 // Output: 8
 // Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
-
 /**
  * @param {number[]} nums
  * @return {number}
