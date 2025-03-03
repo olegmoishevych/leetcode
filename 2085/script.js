a820c2445ace218be3d1db66c3c3a1017fa73c74
Oleh Moishevych <93545466+olegmoishevych@users.noreply.github.com>
2025-03-03 22:06:56 +0200

    Update script.js

--- 2085/script.js
+++ 2085/script.js
@@ -1,5 +1,4 @@
 //Input: words1 = ["leetcode","is","amazing","as","is"], words2 = ["amazing","leetcode","is"]
-
 // Output: 2
 // Explanation:
 // - "leetcode" appears exactly once in each of the two arrays. We count this string.
