ba96adbd2ae52dc7d7c20b8e7c51624e22bb3e02
Oleh Moishevych <93545466+olegmoishevych@users.noreply.github.com>
2025-04-03 22:34:13 +0300

    Update script.js

--- 2652_Sum_Multiples/script.js
+++ 2652_Sum_Multiples/script.js
@@ -4,7 +4,6 @@
 // Hint
 // Given a positive integer n, find the sum of all integers in the range [1, n] inclusive that are divisible by 3, 5, or 7.
 // Return an integer denoting the sum of all numbers in the given range satisfying the constraint.
-
 //   Example 1:
 //
 // Input: n = 7
