8c73d35829e0c1fd6178ea90f1618d7b67962f8b
Oleh Moishevych <93545466+olegmoishevych@users.noreply.github.com>
2025-08-05 23:58:22 +0300

    Update script.js

--- 1281/script.js
+++ 1281/script.js
@@ -4,7 +4,6 @@
 // // Companies
 // // Hint
 // // Given an integer number n, return the difference between the product of its digits and the sum of its digits.
-// //
 // // Example 1:
 // // Input: n = 234
 // // Output: 15
