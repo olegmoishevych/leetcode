eb089c01887bca473d3948366d607435936fb968
Oleh Moishevych <93545466+olegmoishevych@users.noreply.github.com>
2025-08-10 22:33:50 +0300

    Update script.js

--- 1365/script.js
+++ 1365/script.js
@@ -4,7 +4,6 @@
 // Companies
 // Hint
 // Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
-//
 // Return the answer in an array.
 //
 // Example 1:
